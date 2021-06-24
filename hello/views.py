f35de47a04adbeec410fba4fc530541b20da2d57
schnittman <schnittman@users.noreply.github.com>
2021-06-24 16:47:40 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 import requests
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+import html
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Greeting
