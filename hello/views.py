31728d0ae6335910780d579b87b3db41e8f96c2b
ctw599 <46078007+ctw599@users.noreply.github.com>
2021-07-15 13:26:46 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -43,7 +43,7 @@ def form_trial(place, data):
         retval += '<p>Incoming '+place+' data:<br/>\n'
         for key, value in data.items():
             retval += html.escape(key) + '=' + html.escape(value) + '</br>\n'
-            retval += str(double_number(html.escape(value)))
+            retval += str(double_number(int(html.escape(value))))
         retval += '</p>\n'
     return retval
 
@@ -74,7 +74,7 @@ def db(request):
     return render(request, "db.html", {"greetings": greetings})
 
 def double_number(num):
-    return num*2
+    return int(num)*2
 
 @csrf_exempt
 def dateConvert(request):
