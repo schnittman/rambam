79f2e80eb020d1ba8386bb353d339f1eb859a805
ctw599 <46078007+ctw599@users.noreply.github.com>
2021-07-15 13:56:50 -0400

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -42,7 +42,7 @@ def form_trial(place, data):
     if len(data) > 0:
         retval += '<p>Incoming '+place+' data:<br/>\n'
         for key, value in data.items():
-            retval += html.escape(key) + '=' + html.escape(value)'</br>\n'
+            retval += html.escape(key) + '=' + html.escape(value) + '</br>\n'
             #retval += html.escape(key) + '=' + double_number(int(value)) + '</br>\n'
             #retval += str(double_number(html.escape(value)))
         retval += '</p>\n'
